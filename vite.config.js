6fd6a70edddf7bf9d03f4be4822133eeabc41637
WishvanathOfficial <wishvanath.sah.official@gmail.com>
2025-07-12 16:37:38 +0530

    First commit

---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    base: '/career-portfolio/',
+    server: {
+        port: 3000,
+        host: 'localhost',
+        open: true,
+    },
+    plugins: [react()],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        // Split the swiper plugin library into a separate chunk to avoid a large chunk size on index.js
+                        if (id.includes('swiper'))
+                            return 'swiper';
+                        return;
+                    }
+                }
+            }
+        }
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                silenceDeprecations: ["mixed-decls", "color-functions", "global-builtin", "import"],
+            },
+        },
+    },
+})
